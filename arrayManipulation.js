9f0c8ed93a6271d302713ed23dc91d32f6ab985c
ebnyame <aeasiedu002@st.ug.edu.gh>
2024-05-18 17:09:28 -0700

    task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,4 +1,5 @@
-//task
+//task 1
+
 
 function processArray(arr) {
     return arr.map(num => {
@@ -10,4 +11,31 @@ function processArray(arr) {
     });
 }
 
-module.exports = processArray;
+//module.exports = processArray;
+
+
+//Task 2
+
+function formatArrayStrings(stringArray, numberArray) {
+    if (stringArray.length !== numberArray.length) {
+        throw new Error("Both arrays must have the same length");
+    }
+    return stringArray.map((str, index) => {
+        const num = numberArray[index];
+        if (num % 2 === 0) {
+            return str.toUpperCase();
+        } else {
+            return str.toLowerCase();
+        }
+    });
+}
+const inputArray = [1, 2, 3, 4, 5];
+const processedNumbers = processArray(inputArray);
+const stringArray = ["Hello", "World", "JavaScript", "is", "fun"];
+const formattedStrings = formatArrayStrings(stringArray, processedNumbers);
+console.log(formattedStrings);
+
+module.exports = {
+    processArray,
+    formatArrayStrings
+};
